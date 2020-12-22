2e5747690630e9b69771a0544fd7c7663189b0d7
Ashish <ash.ishu025g@gmail.com>
2020-12-22 23:19:58 +0530

    react setup

---
+++ ReactApp/src/app/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+let App = () => {
+    return(
+        <div>
+            <h1>This is react code from react js page</h1>
+        </div>
+    )
+}
+
+export default App;
--- ReactApp/src/index.js
+++ ReactApp/src/index.js
@@ -1,1 +1,10 @@
 console.log("This is the very first react code setup")
+import React from "react";
+import {render} from "react-dom";
+
+import App from "./app/app";
+
+render(
+    <App />,
+    document.getElementById("root") //providing the html element in which we need to inject react application
+)
