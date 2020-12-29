644d7df8b38f5acea56e3481845bcf9a28435675
Ashish <ash.ishu025g@gmail.com>
2020-12-29 23:02:41 +0530

    props

--- ReactApp/src/app/app.js
+++ ReactApp/src/app/app.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header, {Header2, newvar} from "../app/CommonComponent/HeaderComponent";
 //import * as Header from "../app/CommonComponent/HeaderComponent"; //wild card import
+import Footer from "../app/CommonComponent/FooterComponent";
 
 //class component
 
@@ -11,12 +12,21 @@ class App extends React.Component{ //name of the component must start with a cap
     }
 
     render(){
-        return( //the jsx returned must have one top parent
+        let user = {
+            name : "username",
+            pwd : "welcome"
+        }
+        return( //the jsx returned must have one top parent  //react uses diff algorithm to compare
             <React.Fragment> 
-                <h1>This is react code from react js page</h1>
+                <h1>This is react code from react js page MERN</h1>
                 {newvar}
                 <Header/>
                 <Header2/>
+                
+                <Footer name="test name" address="test address" user={user}>
+                    <b>This is bold html passed</b>
+                    <i>This is italic html passed</i>
+                </Footer>
             </React.Fragment>
         )
     }
