18ee95fcc47230ee003ebf46ac80c55b36c9bd5c
Ashish <ash.ishu025g@gmail.com>
2021-01-05 23:25:26 +0530

    router

--- ReactApp/src/app/CommonComponent/HeaderComponent.js
+++ ReactApp/src/app/CommonComponent/HeaderComponent.js
@@ -1,11 +1,16 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
-let Header = ()=>{
+let Header = (props)=>{
+    let userName = "";//props.user.userName;
     return(
         <React.Fragment>
-            <h1>
-                This is going to be header component
-            </h1>
+             Hi <b>{userName +", "}</b> Welcome to SynergisticIT Shopping Cart 
+                {userName == "" ?<b> Please Login to see other features</b>:""}
+            <hr/>
+            <NavLink to="/home" className="button" activeClassName="success" >Home </NavLink> 
+            <NavLink to="/user" className="button" activeClassName="success" >User </NavLink> 
+            <NavLink to="/about" className="button" activeClassName="success" >About </NavLink> 
         </React.Fragment>
     )
 }
--- ReactApp/src/app/app.js
+++ ReactApp/src/app/app.js
@@ -4,46 +4,71 @@ import Header, {Header2, newvar} from "../app/CommonComponent/HeaderComponent";
 //import * as Header from "../app/CommonComponent/HeaderComponent"; //wild card import
 import Footer from "../app/CommonComponent/FooterComponent";
 import Home from "../app/CommonComponent/HomeComponent";
+import About from "../app/CommonComponent/AboutComponent";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
-//class component
-
-class App extends React.Component{ //name of the component must start with a capital case
-    constructor(props, context){
-        super(props);
-        this.state = {
-            childValue : "No child data sent"
-        }
-    }
-
-    getDataFromChild = (data)=>{ //call back function to be executed in child component to read data
-        //alert("data from child = "+ data);
-        this.setState({ //firstName, lastName
-            childValue : data
-        })
-    }
+class App extends React.Component {
 
     render(){
-        let user = {
-            name : "username",
-            pwd : "welcome"
-        }
-        let a = 10, b = 15;
-        return( //the jsx returned must have one top parent  //react uses diff algorithm to compare
-            <React.Fragment> 
-                {/* <h1>This is react code from react js page MERN</h1> */}
-                {a+b}
-                {a*b}
-                <b>{this.state.childValue}</b>
-                <Header/>
-                <Home funcAsProp={this.getDataFromChild}/>
-                <Footer name="test name" address="test address" user={user}>
-                    <b>This is bold html passed</b>
-                    <i>This is italic html passed</i>
-                </Footer>
+        return(
+            <React.Fragment>
+                <Router>
+                    <Header />
+                        <Switch>
+                            <Route path="/home" exact component={Home} />
+                            <Route path="/about" exact component={About} />
+                            <Route path="/about/:id" exact component={About} />
+                            <Route path="/" component={Home} />
+                        </Switch>
+                    <Footer />
+                </Router>
             </React.Fragment>
         )
     }
+
+
 }
+
+
+//class component
+// class App extends React.Component{ //name of the component must start with a capital case
+//     constructor(props, context){
+//         super(props);
+//         this.state = {
+//             childValue : "No child data sent"
+//         }
+//     }
+
+//     getDataFromChild = (data)=>{ //call back function to be executed in child component to read data
+//         //alert("data from child = "+ data);
+//         this.setState({ //firstName, lastName
+//             childValue : data
+//         })
+//     }
+
+//     render(){
+//         let user = {
+//             name : "username",
+//             pwd : "welcome"
+//         }
+//         let a = 10, b = 15;
+//         return( //the jsx returned must have one top parent  //react uses diff algorithm to compare
+//             <React.Fragment> 
+//                 {/* <h1>This is react code from react js page MERN</h1> */}
+//                 {a+b}
+//                 {a*b}
+//                 <b>{this.state.childValue}</b>
+//                 <Header/>
+//                 <Home funcAsProp={this.getDataFromChild}/>
+//                 <About />
+//                 <Footer name="test name" address="test address" user={user}>
+//                     <b>This is bold html passed</b>
+//                     <i>This is italic html passed</i>
+//                 </Footer>
+//             </React.Fragment>
+//         )
+//     }
+// }
 
 //functional component
 // let App = () => {
